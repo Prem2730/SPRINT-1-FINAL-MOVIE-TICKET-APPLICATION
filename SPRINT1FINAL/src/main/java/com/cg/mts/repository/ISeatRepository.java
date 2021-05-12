e97da1973ee08841dd872c6c93712739defabac3
vishnuchakradatta <83800613+vishnuchakradatta@users.noreply.github.com>
2021-05-12 15:57:16 +0530

    Update ISeatRepository.java

--- SPRINT1FINAL/src/main/java/com/cg/mts/repository/ISeatRepository.java
+++ SPRINT1FINAL/src/main/java/com/cg/mts/repository/ISeatRepository.java
@@ -1,5 +1,5 @@
 package com.cg.mts.repository;
-
+//these are packages.
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
