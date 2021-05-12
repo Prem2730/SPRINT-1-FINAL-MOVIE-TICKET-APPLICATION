8a1424d49ec2926cf9ed20ba818171c8f892b437
vishnuchakradatta <83800613+vishnuchakradatta@users.noreply.github.com>
2021-05-12 15:55:33 +0530

    Update SeatExistsException.java

--- SPRINT1FINAL/src/main/java/com/cg/mts/exception/SeatExistsException.java
+++ SPRINT1FINAL/src/main/java/com/cg/mts/exception/SeatExistsException.java
@@ -1,5 +1,5 @@
 package com.cg.mts.exception;
-
+//this is the exception if existing seat not found
 public class SeatExistsException extends RuntimeException {
 	public SeatExistsException(String msg) {
 		// TODO Auto-generated constructor stub
