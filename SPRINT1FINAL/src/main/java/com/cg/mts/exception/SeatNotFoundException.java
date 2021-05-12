8d623705df529e33905d18816db49e1648341d44
vishnuchakradatta <83800613+vishnuchakradatta@users.noreply.github.com>
2021-05-12 15:56:17 +0530

    Update SeatNotFoundException.java

--- SPRINT1FINAL/src/main/java/com/cg/mts/exception/SeatNotFoundException.java
+++ SPRINT1FINAL/src/main/java/com/cg/mts/exception/SeatNotFoundException.java
@@ -1,5 +1,5 @@
 package com.cg.mts.exception;
-
+//Exception occurs if seat not found
 public class SeatNotFoundException extends Exception {
 	public SeatNotFoundException(String message) {
 		super(message);
