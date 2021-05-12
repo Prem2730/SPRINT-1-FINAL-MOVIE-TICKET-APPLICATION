7d783c0dddef814a5a0f952e9862845648c93607
Nagendra345 <83801166+Nagendra345@users.noreply.github.com>
2021-05-12 15:51:22 +0530

    Update Booking.java

--- SPRINT1FINAL/src/main/java/com/cg/mts/entities/Booking.java
+++ SPRINT1FINAL/src/main/java/com/cg/mts/entities/Booking.java
@@ -11,6 +11,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+//this entity class is done by nagendra
 //this entity class is done by raviteja
 @Entity
 @Table(name="booking_master")
